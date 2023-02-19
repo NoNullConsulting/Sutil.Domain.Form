1bbfa2517727a2e33b15ba60545c6bace49e1a2d
allankrn2 <allankrn2@gmail.com>
2023-02-19 21:18:42 +0100

    Add Action project

---
+++ src/Actions/Program.fs
@@ -0,0 +1,23 @@
+﻿open Fake.Core
+
+let clean () = ()
+
+
+
+let initTargets () =
+    // *** Define Targets ***
+    Target.create "Hello" (fun _ -> printfn "hello from FAKE!")
+
+    // *** Start Build ***
+    Target.runOrDefault "Hello"
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> Array.toList
+    |> Context.FakeExecutionContext.Create false "build.fsx"
+    |> Context.RuntimeContext.Fake
+    |> Context.setExecutionContext
+    |> initTargets
+
+    0
